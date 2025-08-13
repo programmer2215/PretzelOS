958bca81a226f2de83184cfa8e68477adc7ae5a1
Mark Madhukar <mark.madhukar@zohocorp.com>
2025-08-13 20:01:56 +0530

    create Makefile & read data from hard disc

--- boot.asm
+++ boot.asm
@@ -10,19 +10,26 @@ times 33 db 0 ; account for BIOS Param Block
 start:
     jmp 0x7c0:step2
 
-handle_zero: ; define interupt 0
-    mov ah, 0eh
-    mov al, 'A'
-    mov bx, 0x00
-    int 0x10
-    iret
 
-handle_one:; define interupt 1
-    mov ah, 0eh
-    mov al, 'V'
-    mov bx, 0x00
-    int 0x10
-    iret
+; DISK - READ SECTOR(S) INTO MEMORY
+
+; AH = 02h
+; AL = number of sectors to read (must be nonzero)
+; CH = low eight bits of cylinder number
+; CL = sector number 1-63 (bits 0-5)
+; high two bits of cylinder (bits 6-7, hard disk only)
+; DH = head number
+; DL = drive number (bit 7 set for hard disk)
+; ES:BX -> data buffer
+
+; Return:
+; CF set on error
+; if AH = 11h (corrected ECC error), AL = burst length
+; CF clear if successful
+; AH = status (see #00234)
+; AL = number of sectors transferred (only valid if CF set for some
+; BIOSes)
+
 
 step2:
     cli ; Clear Interupts
@@ -34,19 +41,26 @@ step2:
     mov sp, 0x7c00
     sti ; start interupts
 
-    mov word[ss:0x00], handle_zero
-    mov word[ss:0x02], 0x7c0
+    mov ah, 2
+    mov al, 1
+    mov ch, 0
+    mov cl, 2
+    mov dh, 0
+    mov bx, buffer
+    int 0x13
 
-    mov word[ss:0x04], handle_one
-    mov word[ss:0x06], 0x7c0
+    jc error
 
-    int 0
-    int 1
-
-    mov si, message
+    mov si, buffer
     call print
+
+    
     jmp $
 
+error:
+    mov si, error_message
+    call print
+    jmp $
 
 print:
     mov bx, 0
@@ -65,8 +79,10 @@ print_chr:
     int 0x10
     ret
 
-message: db "Hello World!", 0
+error_message: db "Failed to load sector", 0
 
 times 510 - ($ - $$) db 0
 
 dw 0xAA55
+
+buffer: 
